99998351ad8dcf24cf30c36d31c5f037e9969702
denissarapov84-dev <denissarapov84@gmail.com>
2025-10-20 17:17:42 +0500

    готово

--- tasks/task1.py
+++ tasks/task1.py
@@ -3,6 +3,9 @@
 def solve():
 # Ниже пишите решение задачи
     
+    word = input()
+    result = word + " " + word + " " + word + " " + word
+    print(result)
         
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
--- tasks/task5.py
+++ tasks/task5.py
@@ -2,7 +2,16 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    symbols = input()
+    char1, char2, char3 = symbols.split()s
+
+    code1 = ord(char1)
+    code2 = ord(char2)
+    code3 = ord(char3)
+
+    print("Код символа", char1, "равен", code1)
+    print("Код символа", char2, "равен", code2)
+    print("Код символа", char3, "равен", code3)
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
